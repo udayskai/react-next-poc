0920dcd239fb4b1cd366adf4c5cb0c1e5210264c
Udayskai <udayskai@gmail.com>
2023-01-11 13:17:13 +0530

    package added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+// module.exports = nextConfig
 
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    let path_array = [
+      {
+        source: "/:path*",
+        destination: "http://localhost:3000/:path*", // Proxy to Backend
+      },
+    ];
+    return path_array;
+  },
+}
